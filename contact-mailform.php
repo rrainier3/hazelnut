f009362216d6d87eb2afd333364a8b2540f14eef
Ray Rainier <rayrainier@yahoo.com>
2017-08-09 22:49:52 -0500

    Update 3.0

--- contact-mailform.php
+++ contact-mailform.php
@@ -1,18 +1,80 @@
 <?php
-// using SendGrid's PHP Library
-// https://github.com/sendgrid/sendgrid-php
-// If you are using Composer (recommended)
 require 'vendor/autoload.php';
-// If you are not using Composer
-// require("path/to/sendgrid-php/sendgrid-php.php");
-$from = new SendGrid\Email("Example User", "test@example.com");
-$subject = "Sending with SendGrid is Fun";
-$to = new SendGrid\Email("Example User", "test@example.com");
-$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
-$mail = new SendGrid\Mail($from, $subject, $to, $content);
-$apiKey = getenv('SENDGRID_API_KEY');
-$sg = new \SendGrid($apiKey);
-$response = $sg->client->mail()->send()->post($mail);
-echo $response->statusCode();
-print_r($response->headers());
-echo $response->body();
+
+// ADD YOUR INFOMATION HERE
+$recipient = "rrainier3@hotmail.com";
+$successPage = "index.html";
+// NO NEED TO EDIT ANYTHING BELOW THIS LINE =====================//
+
+
+//import form information
+$name = $_REQUEST['name'];
+$email = $_REQUEST['email'];
+$subject = $_REQUEST['subject'];
+$message = $_REQUEST['message'];
+
+
+$name=stripslashes($name);
+$email=stripslashes($email);
+$subject=stripslashes($subject);
+$message=stripslashes($message);
+$message= "Name: $name, \n\n Message: $message";
+
+/*
+Simple form validation
+check to see if an email and message were entered
+*/
+//if no message entered and no email entered print an error
+if (empty($message) && empty($email)){
+print "No email address and no message was entered. <br>Please include an email and a message 555";
+}
+//if no message entered send print an error
+elseif (empty($message)){
+print "No message was entered.<br>Please include a message. 444 <br>";
+}
+//if no email entered send print an error
+elseif (empty($email)){
+print "No email address was entered.<br>Please include your email 777. <br>";
+}
+//if the form has both an email and a message
+else {
+
+//mail the form contents
+//mail( "$recipient", "$subject", "$message", "From: $email" );
+	dumpHello($recipient, $subject, $message, $email);
+
+//sendHelloEmail($recipient, $subject, $message, $email);
+
+//header("Location: $successPage");
+}
+
+function dumpHello($recipient, $subject, $message, $email)
+{
+	print("$recipient, $subject, $message, $email");
+}
+
+function helloEmail($recipient, $subject, $message, $email)
+{
+    $from = new Email(null, $email);
+    $subject0 = $subject;
+    $to = new Email(null, $recipient);
+    $content = new Content("text/plain", $message);
+    $mail = new Mail($from, $subject0, $to, $content);
+    $to = new Email(null, "rrainier@hotmail.com");
+    $mail->personalization[0]->addTo($to);
+    //echo json_encode($mail, JSON_PRETTY_PRINT), "\n";
+    return $mail;
+}
+
+function sendHelloEmail($recipient, $subject, $message, $email)
+{
+    $apiKey = getenv('SENDGRID_API_KEY');
+    $sg = new \SendGrid($apiKey);
+    $request_body = helloEmail($recipient, $subject, $message, $email);
+    $response = $sg->client->mail()->send()->post($request_body);
+    echo $response->statusCode();
+    echo $response->body();
+    print_r($response->headers());
+}
+
+?>
