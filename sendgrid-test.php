b585860d8929ee369fc9b11ee353eaf35b3dc6f5
Ray Rainier <rayrainier@yahoo.com>
2017-08-11 17:27:37 -0500

    SG Test 1.4

--- sendgrid-test.php
+++ sendgrid-test.php
@@ -6,9 +6,9 @@ require 'vendor/autoload.php';
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
 $from = new SendGrid\Email("Example User", "rrainier3@hotmail.com");
-$subject = "Sending with SendGrid is Fun";
+$subject = "Sending with SendGrid is Fun 2017";
 $to = new SendGrid\Email("Example User", "rayrainier@yahoo.com");
-$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$content = new SendGrid\Content("text/plain", "this is a round trip test 123");
 $mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
@@ -18,3 +18,5 @@ $response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
 print_r($response->headers());
 echo $response->body();
+
+print_r($response->body());
