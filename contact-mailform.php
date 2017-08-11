7d99ad6ce473150eddafe2579e35dfbf2d5d6a2d
Ray Rainier <rayrainier@yahoo.com>
2017-08-11 07:37:24 -0500

    Back to SG

--- contact-mailform.php
+++ contact-mailform.php
@@ -1,62 +1,37 @@
 <?php
-
-# Include the Autoloader (see "Libraries" for install instructions)
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
-
-// ADD YOUR INFOMATION HERE
-$recipient = "rrainier@hotmail.com";
-$successPage = "index.html";
-// NO NEED TO EDIT ANYTHING BELOW THIS LINE =====================//
-
-
-//import form information
+/*SendGrid Library*/
+require_once ('vendor/autoload.php');
+/*Post Data*/
 $name = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
 $message = $_POST['message'];
+/*Content*/
+$from = new SendGrid\Email("ROYS LAWN SERVICE", "rayrainier@yahoo.com");
 
+// $subject = "SUBJECT";
+$subject = $_POST['message'];
 
-$name=stripslashes($name);
-$email=stripslashes($email);
-$subject=stripslashes($subject);
-$message=stripslashes($message);
-$message= "Name: $name, \n\n Message: $message";
+$to = new SendGrid\Email("PAUL STANLEY", "rrainier3@hotmail.com");
 
-/*
-Simple form validation
-check to see if an email and message were entered
-*/
-//if no message entered and no email entered print an error
-if (empty($message) && empty($email)){
-print "No email address and no message was entered. <br>Please include an email and a message";
-}
-//if no message entered send print an error
-elseif (empty($message)){
-print "No message was entered.<br>Please include a message.<br>";
-}
-//if no email entered send print an error
-elseif (empty($email)){
-print "No email address was entered.<br>Please include your email. <br>";
-}
-//if the form has both an email and a message
-else {
+$content = new SendGrid\Content("text/html", "
 
-//mail the form contents
-// mail( "$recipient", "$subject", "$message", "From: $email" );
-# Instantiate the client.
-$apiKey = getenv('MAILGUN_API_KEY');
-$mgClient = new Mailgun($apiKey);
-$domain = "samples.mailgun.org";
+Email : {$email}<br>
+Name : {$name}<br>
+Message : {$message}
 
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-  array('from'    => 'Excited User <excited@samples.mailgun.org>',
-        'to'      => 'Mailgun Devs <devs@mailgun.net>',
-        'subject' => 'Hello',
-        'text'    => 'Testing some Mailgun awesomeness!'));
+	");
 
-header("Location: $successPage");
-}
-
+/*Send the mail*/
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = ('API_KEY');
+$sg = new \SendGrid($apiKey);
+/*Response*/
+$response = $sg->client->mail()->send()->post($mail);
 ?>
+
+<!--Print the response-->
+<pre>
+    <?php
+    var_dump($response);
+    ?>
+</pre>
