656149f414eb69e18c8a41a87de48188851be655
Ray Rainier <rayrainier@yahoo.com>
2017-08-11 17:17:20 -0500

    SG Test 1.3

--- sendgrid-test.php
+++ sendgrid-test.php
@@ -5,32 +5,16 @@ require 'vendor/autoload.php';
 // If you are not using Composer
 // require("path/to/sendgrid-php/sendgrid-php.php");
 
-$request_body = json_decode('{
-  "personalizations": [
-    {
-      "to": [
-        {
-          "email": "rayrainier@yahoo.com"
-        }
-      ],
-      "subject": "Sending with SendGrid is Fun"
-    }
-  ],
-  "from": {
-    "email": "rrainier3@hotmail.com"
-  },
-  "content": [
-    {
-      "type": "text/plain",
-      "value": "and easy to do anywhere, even with PHP"
-    }
-  ]
-}');
+$from = new SendGrid\Email("Example User", "rrainier3@hotmail.com");
+$subject = "Sending with SendGrid is Fun";
+$to = new SendGrid\Email("Example User", "rayrainier@yahoo.com");
+$content = new SendGrid\Content("text/plain", "and easy to do anywhere, even with PHP");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
 
 $apiKey = getenv('SENDGRID_API_KEY');
 $sg = new \SendGrid($apiKey);
 
-$response = $sg->client->mail()->send()->post($request_body);
+$response = $sg->client->mail()->send()->post($mail);
 echo $response->statusCode();
-echo $response->body();
 print_r($response->headers());
+echo $response->body();
