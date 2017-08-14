4ec11faa03312df46b1ad5431d4f42881a3f4f72
Ray Rainier <rayrainier@yahoo.com>
2017-08-14 08:06:03 -0500

    back to MG

--- mailgunx.php
+++ mailgunx.php
@@ -6,7 +6,7 @@ $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
 echo "Successfully instantiated mailgun!";
 
-$domain = "sandboxbf951da5d01741feaf8cd9c343aca27b.mailgun.org";
+$domain = "mg.usnurses.com";
 
 # Make the call to the client.
 $result = $mailgun->sendMessage($domain, array(
