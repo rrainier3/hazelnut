95651e1c8f37ec955dbc6d8ec93f37d82d247cf4
Ray Rainier <rayrainier@yahoo.com>
2017-08-13 21:51:54 -0500

    back to MG

--- mailgunx.php
+++ mailgunx.php
@@ -2,7 +2,7 @@
 
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-$mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75', new \Http\Adapter\Guzzle6\Client());
+$mailgun = new Mailgun('88072c0127ec9c6e30c7b39de2a74b75', new \Http\Adapter\Guzzle6\Client());
 
 echo "Successfully instantiated mailgun!"
 
