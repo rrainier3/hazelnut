1104c547ab726e00cd1aa429d31b0d88d0b8e4b0
Ray Rainier <rayrainier@yahoo.com>
2017-08-14 07:20:33 -0500

    back to MG

--- mailgunx.php
+++ mailgunx.php
@@ -6,12 +6,12 @@ $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
 echo "Successfully instantiated mailgun!";
 
-$domain = "yahoo.com";
+$domain = "sandboxbf951da5d01741feaf8cd9c343aca27b.mailgun.org";
 
 # Make the call to the client.
 $result = $mailgun->sendMessage($domain, array(
-    'from'    => 'Excited User <rayrainier@yahoo.com>',
-    'to'      => 'Baz <rrainier@hotmail.com>',
+    'from'    => 'Excited User <rrainier@hotmail.com>',
+    'to'      => 'Baz <rayrainier@yahoo.com>',
     'subject' => 'Hello',
     'text'    => 'Testing some Mailgun blah blah!'
 ));
