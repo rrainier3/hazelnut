f3b005fcade22048d1b6b1411bc56d9c9a02e515
Ray Rainier <rayrainier@yahoo.com>
2017-08-13 23:06:34 -0500

    back to MG

--- mailgunx.php
+++ mailgunx.php
@@ -6,12 +6,12 @@ $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
 echo "Successfully instantiated mailgun!";
 
-$domain = "sandboxbf951da5d01741feaf8cd9c343aca27b.mailgun.org";
+$domain = "hotmail.com";
 
 # Make the call to the client.
 $result = $mailgun->sendMessage($domain, array(
-    'from'    => 'Excited User <rrainier@hotmail.com>',
-    'to'      => 'Baz <rayrainier@yahoo.com>',
+    'from'    => 'Excited User <rayrainier@yahoo.com>',
+    'to'      => 'Baz <rrainier@hotmail.com>',
     'subject' => 'Hello',
     'text'    => 'Testing some Mailgun blah blah!'
 ));
