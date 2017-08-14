acda7523d8e284ec779a5d17f775d6698a1a8cd4
Ray Rainier <rayrainier@yahoo.com>
2017-08-13 22:32:11 -0500

    back to MG

--- mailgunx.php
+++ mailgunx.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
-echo "Successfully instantiated mailgun!"
+echo "Successfully instantiated mailgun!";
 
 $domain = "example.com";
 
