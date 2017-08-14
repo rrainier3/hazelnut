8db9489d84ecf57d9421daa48fd88a6b419316e8
Ray Rainier <rayrainier@yahoo.com>
2017-08-13 23:01:50 -0500

    back to MG

--- mailgunx.php
+++ mailgunx.php
@@ -6,7 +6,7 @@ $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
 echo "Successfully instantiated mailgun!";
 
-$domain = "hotmail.com";
+$domain = "sandboxbf951da5d01741feaf8cd9c343aca27b.mailgun.org";
 
 # Make the call to the client.
 $result = $mailgun->sendMessage($domain, array(
