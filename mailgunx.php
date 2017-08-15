8a49e8c4ad140b2fa439c222691ccfcf598d6826
Ray Rainier <rayrainier@yahoo.com>
2017-08-15 07:19:18 -0500

    Back to MG 1.4

--- mailgunx.php
+++ mailgunx.php
@@ -2,7 +2,11 @@
 
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-$mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
+
+// $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
+
+$mg_api_key = getenv('MAILGUN_API_KEY')
+$mailgun = new Mailgun($mg_api_key)
 
 echo "Successfully instantiated mailgun!";
 
@@ -11,9 +15,9 @@ $domain = "mg.usnurses.com";
 # Make the call to the client.
 $result = $mailgun->sendMessage($domain, array(
     'from'    => 'RAY RAY <rayrainier@usnurses.com>',
-    'to'      => 'Roy <roywesley@sbcglobal.net>',
+    'to'      => 'Proximity <roywesley@sbcglobal.net>',
     'subject' => 'RE: Questions on Roys Lawn Services',
-    'text'    => 'Hey Roy this is Ray testing our email form'
+    'text'    => 'This is a test for getenv var MAILGUN_API_KEY'
 ));
 
 ?>
