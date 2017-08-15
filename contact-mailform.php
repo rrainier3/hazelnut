59f5744742db8783391d4a4d4cf7914ba7205567
Ray Rainier <rayrainier@yahoo.com>
2017-08-15 07:43:53 -0500

    Back to MG 1.5

--- contact-mailform.php
+++ contact-mailform.php
@@ -2,26 +2,16 @@
 // If you are using Composer (recommended)
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
-$mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
-// does not work preconfig on heroku!
-// $apiKey = getenv('MAILGUN_API_KEY');
-// $mailgun = new Mailgun($apiKey);
+$mg_api_key = getenv('MAILGUN_API_KEY');
+$mailgun = new Mailgun($mg_api_key);
 
-echo "Successfully instantiated mailgun!";
+// echo "Successfully instantiated mailgun!";
 
 $domain = "mg.usnurses.com";
 
-// # Make the call to the client.
-// $result = $mailgun->sendMessage($domain, array(
-//     'from'    => 'RAY RAY <rayrainier@usnurses.com>',
-//     'to'      => 'Roy <roywesley@sbcglobal.net>',
-//     'subject' => 'RE: Questions on Roys Lawn Services',
-//     'text'    => 'Hey Roy this is Ray testing our email form'
-// ));
-
-// ADD YOUR INFOMATION HERE
-$recipient = "rrainier3@hotmail.com";
+// ADD OWNER INFOMATION HERE
+$businessowner = "rrainier3@hotmail.com";
 $successPage = "index.html";
 // NO NEED TO EDIT ANYTHING BELOW THIS LINE =====================//
 
@@ -37,7 +27,7 @@ $name=stripslashes($name);
 $email=stripslashes($email);
 $subject=stripslashes($subject);
 $message=stripslashes($message);
-$message= "Name: $name, \n\n Message: $message";
+$message= "Name: " . $name . " <" . $email . ">" . ", \n\n Message: $message";
 
 /*
 Simple form validation
@@ -60,8 +50,8 @@ else {
 
 # Make the call to the client.
 $result = $mailgun->sendMessage("$domain",
-  array('from'    => 'Roys Lawn Service <royslawnservice@usnurses.com>',
-        'to'      => 'Customer <' . $recipient . '>',
+  array('from'    => 'System Admin <royslawnservice@usnurses.com>',
+        'to'      => 'Roys Lawn Service <' . $businessowner . '>',
         'subject' => $subject,
         'text'    => $message));
 
--- mailgunx.php
+++ mailgunx.php
@@ -3,8 +3,6 @@
 require 'vendor/autoload.php';
 use Mailgun\Mailgun;
 
-// $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
-
 $mg_api_key = getenv('MAILGUN_API_KEY');
 $mailgun = new Mailgun($mg_api_key);
 
