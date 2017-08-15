4935cb1eb17838db876508549bb0cae4cbe75cf4
Ray Rainier <rayrainier@yahoo.com>
2017-08-15 07:23:52 -0500

    Back to MG 1.5

--- mailgunx.php
+++ mailgunx.php
@@ -5,8 +5,8 @@ use Mailgun\Mailgun;
 
 // $mailgun = new Mailgun('key-88072c0127ec9c6e30c7b39de2a74b75');
 
-$mg_api_key = getenv('MAILGUN_API_KEY')
-$mailgun = new Mailgun($mg_api_key)
+$mg_api_key = getenv('MAILGUN_API_KEY');
+$mailgun = new Mailgun($mg_api_key);
 
 echo "Successfully instantiated mailgun!";
 
